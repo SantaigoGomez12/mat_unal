6e73b190b7c6f04bda377e14bccd5ebc649bb73b
Santiago Gomez <sgomezsalazar7@gmail.com>
2022-11-16 15:16:23 -0500

    Wed Nov 16 03:16:12 PM -05 2022

--- 26.js
+++ 26.js
@@ -14,7 +14,7 @@ function ang_refe (angulo) {
 
     return third
 }
-console.log(ang_refe(25000));
+console.log(ang_refe(33105));
 
 // Radianes a grados
 function rad_gra (nume, deno) {
@@ -28,24 +28,25 @@ function ang_terminal (angulo) {
     let vuletas = new_angle * 360;
     return angulo - vuletas
 }
-console.log(ang_terminal(23573));
+console.log(ang_terminal(33794));
 
 // Posicion estandar
 function tan_cos (tan) {
     value1 = Math.atan(tan);
     return Math.cos(value1).toFixed(5)
 }
-console.log(tan_cos(-1/31));
+console.log(tan_cos(-1/12));
 
 function sec_sen (sec) {
     let value1 = math.asec(sec)
     return Math.sin(value1).toFixed(5)
 }
-console.log(sec_sen(-26));
+console.log("Este es el resultado");
+console.log(sec_sen(50));
 
 function cot_cos (cot) {
     let value1 = math.acot(cot).toFixed(4)
     console.log(value1);
-    return Math.cos(value1).toFixed(5)
+    return math.cos(value1).toFixed(5)
 }
-console.log(cot_cos(1/25));
+console.log(cot_cos(-1/31));
--- 27.js
+++ 27.js
@@ -8,4 +8,29 @@ function helicopter (metros, medida1, medida2) {
     
     return [res_1, res_2, altura]
 }
-console.log(helicopter(280, 37, 27));
+console.log(helicopter(230, 38, 33));
+
+// Lado de paralelorgamo 
+function para (lado1, lado2, angulo) {
+    let h = (lado1 * Math.sin(math.unit(angulo, 'deg').value))
+    let area = lado2 * h;
+    return area.toFixed(2)
+}
+console.log(para(29,30,31));
+
+//  Valores triangulo rojo
+function tri_roj (a, bb) {
+    let r = a / (Math.cos(math.unit(bb, 'deg').value));
+    r = (r - 0.02).toFixed(2)
+    let b = (Math.sqrt((r ** 2) - (a ** 2)))
+    let angle_a = Math.acos(((a / r) - 0.01).toFixed(2))
+    angle_a = (angle_a * Math.PI) / 180
+    return [r, b, angle_a]
+}
+console.log(tri_roj(9, 65));
+
+// lado paralelogramo 
+function avion (metros, angulo) {
+    return metros / (Math.sin(math.unit(angulo, 'deg').value))
+}
+console.log(avion(1400, 22));
